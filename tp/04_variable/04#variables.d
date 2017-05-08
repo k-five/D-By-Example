3931c1d23518da4b5b711cf8fd4619c08067aa24
Shekib <vhsu30@yahoo.com>
2017-05-08 14:20:20 +0430

    variable in D

---
+++ tp/04_variable/04#variables.d
@@ -0,0 +1,26 @@
+import std.stdio;
+
+void main( ){
+	bool b = true;
+	byte b2;
+	char ch;
+	int i;
+	uint ui;
+	short sh;
+	ushort ush;
+	long l;
+	ulong ul;
+	
+	float f;
+	ifloat i_f;
+	double d = 33.3;
+	idouble id;
+	writeln( "double: ", d );
+}
+
+/* the output: 
+ 
+double: 33.3
+
+/*
+
