c86c4142e8a08205707fa23ce698b98f95389bc0
Shekib <vhsu30@yahoo.com>
2017-05-08 14:20:09 +0430

    basic syntax in D

---
+++ tp/03_basic_syntax/03#basic_rules.d
@@ -0,0 +1,11 @@
+import std.stdio;
+
+void main( ){
+	// basic syntax:
+	//
+	// comments
+	/* and comments */
+	// import: for importing the library that is used
+	// case-sensitive
+	// free-whitespace
+}
