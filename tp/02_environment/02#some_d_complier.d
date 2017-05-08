32b3feb136fa111562962b07d440bfc138f3b6dd
Shekib <vhsu30@yahoo.com>
2017-05-08 14:19:37 +0430

    environment

---
+++ tp/02_environment/02#some_d_complier.d
@@ -0,0 +1,7 @@
+import std.stdio;
+
+void main( string[] args ){
+	writeln( "DMD:\tThe Digital Mars D complier is the official D complier written by Walter Bright" );
+	writeln( "GDC:\tA front-end of the GCC back-end, build using the open DMD complier source code" );
+	writeln( "LDC:\tA compiler based on DMD front-end that uses LLVM as it complier back-end" );
+}
